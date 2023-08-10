a3c493459439aa1830c6ad8fb13a37c0a2efac85
Vishal Kumar <109166596+vis-hal-k@users.noreply.github.com>
2023-08-11 00:09:06 +0530

    Added solution - LeetHub

---
+++ C++ Strings - GFG/c-strings.cpp
@@ -0,0 +1,29 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<string>
+using namespace std;
+
+string conCat(string a , string b);
+
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        string a,b;
+        cin>>a>>b;
+        cout<<conCat(a,b)<<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+string conCat(string s1 , string s2)
+{
+    // code here.
+    string concat = s1 + s2 ;
+    return concat ;
+}
