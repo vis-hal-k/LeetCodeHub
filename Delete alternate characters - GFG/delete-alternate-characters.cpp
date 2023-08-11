43dc19adc4b64ed361fa95419ec0b2e0a9c0b22b
Vishal Kumar <109166596+vis-hal-k@users.noreply.github.com>
2023-08-11 11:02:29 +0530

    Added solution - LeetHub

---
+++ Delete alternate characters - GFG/delete-alternate-characters.cpp
@@ -0,0 +1,33 @@
+//{ Driver Code Starts
+// Initial Template for C++
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+// User function Template for C++
+class Solution {
+  public:
+    string delAlternate(string S) {
+        // code here
+        string str = "" ;
+        for(int i = 0 ; i<S.size() ; i=i+2) {
+            str += S[i] ;
+        }
+        return str ;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        string S;
+        cin >> S;
+        Solution ob;
+        cout << ob.delAlternate(S) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
